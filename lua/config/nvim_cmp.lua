e91a7485bedf437f51ffc6a845e69edc6584e7b9
htlsne <htlsne23@gmail.com>
2022-01-27 00:29:21 +0900

    Update nvim-lsp

--- lua/config/nvim_cmp.lua
+++ lua/config/nvim_cmp.lua
@@ -22,7 +22,6 @@ cmp.setup({
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
---   capabilities = capabilities
--- }
+require('lspconfig')['tsserver'].setup {
+  capabilities = capabilities
+}
