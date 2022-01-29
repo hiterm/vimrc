84baf4c9511e2687ff070ad69dbc3eed9f72f486
htlsne <htlsne23@gmail.com>
2022-01-29 14:33:40 +0900

    Refactor lua settings

--- lua/config/nvim_cmp.lua
+++ lua/config/nvim_cmp.lua
@@ -106,6 +106,33 @@ local on_attach = function(client, bufnr)
   lsp_status.on_attach(client)
 end
 
+-- option for lua
+local runtime_path = vim.split(package.path, ';')
+local lua_opts = {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities = vim.tbl_extend('keep', capabilities, lsp_status.capabilities)
 
@@ -117,34 +144,8 @@ lsp_installer.on_server_ready(function(server)
 
   -- (optional) Customize the options passed to the server
   if server.name == "sumneko_lua" then
-    local runtime_path = vim.split(package.path, ';')
     table.insert(runtime_path, "lua/?.lua")
     table.insert(runtime_path, "lua/?/init.lua")
-
-    local lua_opts = {
-      settings = {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-          },
-          -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    }
 
     opts = vim.tbl_extend('keep', opts, lua_opts)
   end
