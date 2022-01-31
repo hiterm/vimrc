4f740392cb1e7008751c8ddd812e04878f978cbd
htlsne <htlsne23@gmail.com>
2022-01-31 21:18:37 +0900

    Remove treesitter textsubjects

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -24,13 +24,6 @@ require("nvim-treesitter.configs").setup({
 		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 		max_file_lines = nil, -- Do not enable for files with more than n lines, int
 	},
-	textsubjects = {
-		enable = true,
-		prev_selection = ",", -- (Optional) keymap to select the previous selection
-		keymaps = {
-			["."] = "textsubjects-smart",
-		},
-	},
 	textobjects = {
 		select = {
 			enable = true,
