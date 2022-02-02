34a0485da1ca99f87bccdf5e87a3b24c11317666
htlsne <htlsne23@gmail.com>
2022-02-02 21:50:21 +0900

    Enable syntax highlight for vim in toml

---
+++ after/queries/toml/injections.scm
@@ -0,0 +1,24 @@
+((pair
+  (bare_key) @_key
+  (string) @vim)
+ (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @vim "^\"\"\"")
+ (#offset! @vim 0 3 0 -3))
+((pair
+  (bare_key) @_key
+  (string) @vim)
+ (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @vim "^'''")
+ (#offset! @vim 0 3 0 -3))
+((pair
+  (bare_key) @_key
+  (string) @vim)
+ (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @vim "^'\([^']\)\@=")
+ (#offset! @vim 0 1 0 -1))
+((pair
+  (bare_key) @_key
+  (string) @vim)
+ (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @vim "^\"\([^\"]\)\@=")
+ (#offset! @vim 0 1 0 -1))
