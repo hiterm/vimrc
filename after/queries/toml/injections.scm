2e026029caa1890b474624d513ce25a6f870dbd4
htlsne <htlsne23@gmail.com>
2022-02-02 22:00:44 +0900

    Add comments

--- after/queries/toml/injections.scm
+++ after/queries/toml/injections.scm
@@ -1,24 +1,28 @@
+;; for """vim script"""
 ((pair
   (bare_key) @_key
   (string) @vim)
  (#vim-match? @_key "^hook_\w*")
  (#vim-match? @vim "^\"\"\"")
  (#offset! @vim 0 3 0 -3))
+;; for '''vim script'''
 ((pair
   (bare_key) @_key
   (string) @vim)
  (#vim-match? @_key "^hook_\w*")
  (#vim-match? @vim "^'''")
  (#offset! @vim 0 3 0 -3))
-((pair
-  (bare_key) @_key
-  (string) @vim)
- (#vim-match? @_key "^hook_\w*")
- (#vim-match? @vim "^'\([^']\)\@=")
- (#offset! @vim 0 1 0 -1))
+;; for "vim script"
 ((pair
   (bare_key) @_key
   (string) @vim)
  (#vim-match? @_key "^hook_\w*")
  (#vim-match? @vim "^\"\([^\"]\)\@=")
+ (#offset! @vim 0 1 0 -1))
+;; for 'vim script'
+((pair
+  (bare_key) @_key
+  (string) @vim)
+ (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @vim "^'\([^']\)\@=")
  (#offset! @vim 0 1 0 -1))
